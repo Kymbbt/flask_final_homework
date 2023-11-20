032c22cd28bc47964dbdf629586a950907197ff7
KymbatBaibaturova <kymbat.020293@gmail.com>
2023-11-19 20:29:54 -0500

    final flask

---
+++ car_inventory/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask 
+from flask_migrate import Migrate 
+from flask_cors import CORS 
+from flask_jwt_extended import JWTManager 
+
+
+
+from .blueprints.site.routes import site 
+from .blueprints.auth.routes import auth
+from .blueprints.api.routes import api 
+from config import Config 
+from .models import login_manager, db 
+from .helpers import JSONEncoder
+
+app = Flask(__name__) 
+app.config.from_object(Config)
+jwt = JWTManager(app)
+
+
+login_manager.init_app(app)
+login_manager.login_view = 'auth.sign_in'
+login_manager.login_message = "Hey you! Log in please!"
+login_manager.login_message_category = 'warning'
+
+
+#we are going to use a decorator to create our first route
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello World!</p>"
+
+
+app.register_blueprint(site)
+app.register_blueprint(auth)
+app.register_blueprint(api)
+
+
+db.init_app(app)
+migrate = Migrate(app, db)
+app.json_encoder = JSONEncoder
+cors = CORS(app)
---
+++ config.py
@@ -0,0 +1,42 @@
+from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+# class Config():
+
+#     """
+#     Set Config variables for our flask app.
+#     Using Environment variables where available otherwise
+#     Create config variables.
+
+#     """
+
+#     FLASK_APP = os.environ.get('FLASK_APP')
+#     FLASK_ENV = os.environ.get('FLASK_ENV')
+#     FLASK_DEBUG = True
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or "Don't tell secrets"
+
+class Config():
+
+    """
+    Create Config class which will setup our configuration variables.
+    Using Environment variables where available other create config variables. 
+    """
+
+    #regular configuration for Flask App
+    FLASK_APP = os.environ.get('FLASK_APP') 
+    FLASK_ENV = os.environ.get('FLASK_ENV') 
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+
+    #configuration if you are connecting a database
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Literally whatever you want as long as its a string. Cool Beans'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=365)
